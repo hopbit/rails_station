95d6d9829bff120a94e51120d7e3aa204ce5c6fe
Lukasz Siwinski <lsiwinski@gmail.com>
2016-07-15 09:03:44 +0200

    02_spec.rb:25 - it shows details of a single day at /special_days/:id

--- app/controllers/special_days_controller.rb
+++ app/controllers/special_days_controller.rb
@@ -1,5 +1,11 @@
 class SpecialDaysController < ApplicationController
+
   def index
     @events = SpecialDay.all
   end
+
+  def show
+    @special_day = SpecialDay.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'homepage#index'
   get '/dashboard/:name' => 'dashboard#index'
   get '/special_days' => 'special_days#index'
+  get '/special_days/:id' => 'special_days#show'
   # get '/about' => 'static_pages#about'
 end
