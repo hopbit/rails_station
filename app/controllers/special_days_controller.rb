e23cedc944e42290767489605045ac0da04b38fa
Lukasz Siwinski <lsiwinski@gmail.com>
2016-07-21 06:42:52 +0200

    03_spec.rb:11,20 - it creates new database record when form is submitted & redirects to the list of special days when form is submitted

--- app/controllers/special_days_controller.rb
+++ app/controllers/special_days_controller.rb
@@ -12,4 +12,13 @@ class SpecialDaysController < ApplicationController
     @special_day = SpecialDay.new
   end
 
+  def create
+    special_day_hash = params[:special_day]
+    puts "--> special_day_hash: #{special_day_hash}" # debug
+    if special_day_hash[:name] != nil && special_day_hash[:when] != nil
+      when_date = DateTime.parse(special_day_hash[:when])
+      puts "> when_date: #{when_date}"
+      SpecialDay.create!(name: special_day_hash[:name], when: when_date)
+    end
+  end
 end
