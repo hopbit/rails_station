33988b90fb7bd76a30a4e44ff5463fa9297d47e8
Lukasz Siwinski <lsiwinski@gmail.com>
2016-07-22 06:52:21 +0200

    03_spec.rb:33 - it does not allow special days without date

--- app/models/special_day.rb
+++ app/models/special_day.rb
@@ -1,5 +1,6 @@
 class SpecialDay < ActiveRecord::Base
   validates_presence_of :name
+  validates_presence_of :when
   def to_s
     when_str = self.when ? self.when.strftime('%d-%m-%Y') : ''
     return "#{self.name} - #{when_str}"
