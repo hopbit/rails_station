4a1717a014719dfda9cb4140a298da53a520aaaa
Lukasz Siwinski <lsiwinski@gmail.com>
2016-07-25 06:19:32 +0200

    04_spec.rb:4 - it has Comment model with content field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711190224) do
+ActiveRecord::Schema.define(version: 20160725041514) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "special_days", force: :cascade do |t|
     t.string   "name"
