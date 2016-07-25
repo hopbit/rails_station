4761a7e117493ce2cd5145c628f6718080520c77
Lukasz Siwinski <lsiwinski@gmail.com>
2016-07-25 06:12:26 +0200

    03_spec.rb:38 - it shows errors when form is submitted

--- app/controllers/special_days_controller.rb
+++ app/controllers/special_days_controller.rb
@@ -16,10 +16,12 @@ class SpecialDaysController < ApplicationController
     special_day_hash = params[:special_day]
     name_str = special_day_hash[:name]
     when_date = special_day_hash[:when]
-    if SpecialDay.create!(name: name_str, when: when_date)
+    @special_day = SpecialDay.new(name: name_str, when: when_date)
+    if @special_day.valid? and @special_day.save!
       redirect_to special_days_path
     else
       render :new
     end
   end
+
 end
